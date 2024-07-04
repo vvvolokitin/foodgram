00a44a2c5a45eb59f8dd6c035771423411de6d9d
Nikita Volokitin <n.a.volokitin@gmail.com>
2024-07-04 19:44:19 +0500

    add docker-compose.production

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
-# STATIC_ROOT = BASE_DIR / 'collected_static'
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ backend/recipes/management/commands/importdata.py
@@ -0,0 +1,25 @@
+import csv
+
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = 'Импорт данных из csv файлов в БД.'
+
+    def handle(self, *args, **options):
+        self.import_ingredients()
+
+    def import_ingredients(self):
+        file_path = 'data/ingredients.csv'
+
+        with open(file_path, 'r', encoding='utf-8') as csv_file:
+            reader = csv.reader(csv_file)
+            next(reader)
+
+            for row in reader:
+                ingredient = Ingredient.objects.create(
+                    name=row[0],
+                    measurement_unit=row[1]
+                )
+                ingredient.save()
