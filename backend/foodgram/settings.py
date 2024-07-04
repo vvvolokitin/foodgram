341799c427412bb37b54d62d8c178b971ebcd373
Nikita Volokitin <n.a.volokitin@gmail.com>
2024-07-04 18:30:03 +0500

    test

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -1,14 +1,19 @@
-from pathlib import Path
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = 'django-insecure-y%kw3isoi(zs6k_^nsqf@e(--kqqfhdu0ji0elbe^9rntamtc-'
+SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='localhost').split(', ')
 
 
 INSTALLED_APPS = [
@@ -65,12 +70,36 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('POSTGRESQL', 'django.db.backends.sqlite3'),
     }
 }
+
+if 'POSTGRESQL' in os.environ:
+    DATABASES['default'].update(
+        {
+            'NAME': os.getenv('POSTGRES_DB', 'django'),
+            'USER': os.getenv('POSTGRES_USER', 'django'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+            'HOST': os.getenv('DB_HOST', ''),
+            'PORT': os.getenv('DB_PORT', 5432)
+        }
+    )
+else:
+    DATABASES['default'].update(
+        {
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    )
+
 
 
 AUTH_PASSWORD_VALIDATORS = [
