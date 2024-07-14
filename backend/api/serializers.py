a7c9c3a3f4b995a10c138c5e54d11d68bfbd8051
Nikita Volokitin <n.a.volokitin@gmail.com>
2024-07-14 21:22:03 +0500

    Fix flake8

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -38,17 +38,17 @@ class UserSerializer(DjoserUserSerializer):
             and Subscription.objects.filter(author=obj, user=user).exists()
         )
 
-    
 
 class UserCreateSerializer(DjoserUserSerializer):
     """Сериализатор сздания пользователя."""
 
     class Meta:
         model = User
-        fields = tuple(User.REQUIRED_FIELDS) 
-    
+        fields = tuple(User.REQUIRED_FIELDS)
+
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
 
 class AvatarSerializer(serializers.Serializer):
     """Сериализатор аватара."""
