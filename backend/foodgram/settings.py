24595a79dc63aefe00b522f9ca0e925d0eb4532e
Nikita Volokitin <n.a.volokitin@gmail.com>
2024-07-16 15:48:56 +0500

    Test nginx

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -121,7 +121,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
