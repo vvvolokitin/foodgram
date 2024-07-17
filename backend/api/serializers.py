cf36cdf820f812b5d337f7f98feb4b15d302a731
Nikita Volokitin <n.a.volokitin@gmail.com>
2024-07-17 19:02:52 +0500

    Test Avatar

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -73,14 +73,23 @@ class UserCreateSerializer(DjoserUserSerializer):
         return User.objects.create_user(**validated_data)
 
 
-class AvatarSerializer(serializers.ModelSerializer):
+class AvatarSerializer(serializers.Serializer):
     """Сериализатор аватара."""
 
     avatar = Base64ImageField()
 
-    class Meta:
-        model = User
-        fields = ('avatar',)
+    def update(self, instance, validated_data):
+        instance.avatar = validated_data.get('avatar', instance.avatar)
+        return instance
+
+# class AvatarSerializer(serializers.ModelSerializer):
+#     """Сериализатор аватара."""
+
+#     avatar = Base64ImageField()
+
+#     class Meta:
+#         model = User
+#         fields = ('avatar',)
 
 
 class IngredientSerializer(serializers.ModelSerializer):
