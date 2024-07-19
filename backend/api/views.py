9212787d8172c6adeaab2c445cd0c9f58b977a9a
Nikita Volokitin <n.a.volokitin@gmail.com>
2024-07-19 17:37:34 +0500

    test

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,5 @@
 import os
 
-from dotenv import load_dotenv
 from django.contrib.auth import get_user_model
 from django.db.models import Sum
 from django.http import Http404, HttpResponse
@@ -45,7 +44,6 @@ from recipes.models import (
 from users.models import Subscription
 
 
-load_dotenv()
 User = get_user_model()
 
 
